fd63ad5eb5060eec5f293a795120843f0a833bb1
Peter Stone <peter.stone.online@gmail.com>
2018-09-27 00:23:48 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+
   the_manifest = Hash.new 
 
   # fill that hash with key/value pairs that describe the following information: 
